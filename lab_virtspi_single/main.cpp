1bd80548efd6d8cb884c64fd78d9b89eda4375db
WIDEMAN <mwideman@cocsce-l3d22-01.ds.sc.edu>
2020-03-30 12:21:09 -0400

    second commit

--- lab_virtspi_single/main.cpp
+++ lab_virtspi_single/main.cpp
@@ -1,11 +1,9 @@
 /* Copyright 2020 Jason Bakos, Philip Conrad, Charles Daniels */
 
-/* Used and modified by Charles Daniels for CSCE317, Project lab_virtspi_single
+/* Used and modified by Marquis Wideman for CSCE317, Project lab_virtspi_single
  *
  * HINT: your name is not "Charles Daniels"
  */
-
-//Marquis Wideman
 #include "support.h"
 
 /* number of SPI SCK cycles per system clock cycle */
@@ -14,6 +12,14 @@
 int main(int argc, char **argv) {
 
 	simulation_state* s = initialize_simulation(argc, argv);
+
+	read_io(s, IO_MISO);
+	for (int i = 0; i < 10; i++){
+		write_io(s, IO_SCK, 1);
+		delay_cycles(s, 4);
+		write_io(s, IO_SCK, 0);
+		delay_cycles(s, 4);
+	}
 
 	/* your code here */
 
