5c4f58937bb061483bce2253a0199e07ef5f5178
WIDEMAN <mwideman@cocsce-l3d22-01.ds.sc.edu>
2020-04-03 11:26:35 -0400

    Second Commit

--- lab_virtspi_single/main.cpp
+++ lab_virtspi_single/main.cpp
@@ -13,6 +13,73 @@ int main(int argc, char **argv) {
 
 	simulation_state* s = initialize_simulation(argc, argv);
 
+	write_io(s, IO_SCK, 1);
+	//pull CS high
+
+
+	uint8_t bit = 0;
+	uint8_t data = 0;
+
+	delay_cycles(s, SPI_CLK_RATIO/2);
+
+	//Write register for WHO_AM_I register
+	for (int i = 7; i >=0; i--)
+	{
+		bit = ((0x0f) & (1 << i)) != 0;
+
+		//bit = ((0x0f) & (1 << i )) >> i;
+		//printf("myData at bit %i is %i\n", i, bit);
+
+		//pull SCK low
+		write_io(s, IO_SCK, 0);
+
+		//send bit
+		write_io(s, IO_MOSI, bit);
+
+		//delay SPI_CLK_RATIO
+		delay_cycles(s, SPI_CLK_RATIO/2);
+		
+		//pull SCK high
+		write_io(s, IO_SCK, 1);
+		
+		//delay SPI_CLK_RATIO
+		delay_cycles(s, SPI_CLK_RATIO/2);
+		
+	}
+
+	//delay SPI_CLK_RATIO
+	delay_cycles(s, SPI_CLK_RATIO/2);
+
+	uint8_t whoAmI = 0;
+
+	//Read data in WHO_AM_I register
+	for (int i = 7; i >=0; i--)
+	{
+		//pull SCK low
+		write_io(s, IO_SCK, 0);
+
+		//delay SPI_CLK_RATIO
+		delay_cycles(s, SPI_CLK_RATIO/2);
+
+		whoAmI |= (read_io(s, IO_MISO) << i);
+
+		//pull SCK high
+		write_io(s, IO_SCK, 1);
+
+		//delay SPI_CLK_RATIO
+		delay_cycles(s, SPI_CLK_RATIO/2);
+
+		//bit = (myData & (1 << i)) != 0;
+		//printf("myData at bit %i is %i\n", i, bit);
+	}
+
+	//delay SPI_CLK_RATIO
+	delay_cycles(s, SPI_CLK_RATIO/2);
+
+
+	printf("whoAMI is: %02x\n", whoAmI);
+
+	/*
 	read_io(s, IO_MISO);
 	for (int i = 0; i < 10; i++){
 		write_io(s, IO_SCK, 1);
@@ -20,6 +87,7 @@ int main(int argc, char **argv) {
 		write_io(s, IO_SCK, 0);
 		delay_cycles(s, 4);
 	}
+	*/
 
 	/* your code here */
 
